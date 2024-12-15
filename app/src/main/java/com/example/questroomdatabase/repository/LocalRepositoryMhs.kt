751a0fc166824206028b5f26627a94c00dd324a3
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:06:22 +0700

    LocalRepositoryMhs mengimplementasikan RepositoryMhs untuk menjalankan operasi CRUD tabel Mahasiswa menggunakan MahasiswaDao, memisahkan logika data dari layer lain seperti ViewModel.

---
+++ app/src/main/java/com/example/questroomdatabase/repository/LocalRepositoryMhs.kt
@@ -0,0 +1,31 @@
+package com.example.questroomdatabase.repository
+
+import com.example.questroomdatabase.Data.dao.MahasiswaDao
+import com.example.questroomdatabase.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMhs(
+private val mahasiswaDao: MahasiswaDao
+) : RepositoryMhs {
+
+
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+}
