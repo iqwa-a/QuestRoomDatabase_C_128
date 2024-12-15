e0ed602f23e0d46b4f823728ea9a605b3442f358
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:33:39 +0700

    ViewModel untuk mengelola data Mahasiswa dan logika UI

---
+++ app/src/main/java/com/example/questroomdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.questroomdatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
---
+++ app/src/main/java/com/example/questroomdatabase/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.questroomdatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+// ViewModel untuk mengelola data Mahasiswa dan logika UI
+class MahasiswaViewModel(private val repositoryMhs: RepositoryMhs) : ViewModel() {
