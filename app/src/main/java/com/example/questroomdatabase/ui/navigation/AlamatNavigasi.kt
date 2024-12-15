d3f796c2816bf41924ea2eb037affa0c97360983
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:15:38 +0700

    Menambahkan objek navigasi untuk mendefinisikan rute aplikasi dengan argumen dinamis menggunakan AlamatNavigasi.

---
+++ app/src/main/java/com/example/questroomdatabase/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.questroomdatabase.ui.navigation
+
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+
+object DetinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
