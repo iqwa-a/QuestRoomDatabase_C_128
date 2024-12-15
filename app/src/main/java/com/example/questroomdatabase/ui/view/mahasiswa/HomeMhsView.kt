18e0479d87a7c9c25f287ae9eba897e5ba17f23e
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:31:11 +0700

    ViewModel untuk mengelola data Mahasiswa dan logika UI

--- app/src/main/java/com/example/questroomdatabase/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/questroomdatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -24,6 +24,8 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -31,6 +33,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questroomdatabase.Data.entity.Mahasiswa
+import com.example.questroomdatabase.ui.viewmodel.HomeMhsViewModel
 import kotlinx.coroutines.launch
 
 @Composable
