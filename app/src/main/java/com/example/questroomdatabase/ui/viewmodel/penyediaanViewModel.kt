73739ac1e959f1c171890fabef6286e6260e6171
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:48:15 +0700

    Menambahkan ekstensi KrsApp() pada CreationExtras untuk mengakses instance KrsApp melalui ViewModelProvider.

--- app/src/main/java/com/example/questroomdatabase/ui/viewmodel/penyediaanViewModel.kt
+++ app/src/main/java/com/example/questroomdatabase/ui/viewmodel/penyediaanViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.questroomdatabase.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 
@@ -33,3 +35,5 @@ object penyediaViewModel {
         }
     }
 }
+fun CreationExtras.KrsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
