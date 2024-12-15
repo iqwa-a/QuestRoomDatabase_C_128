25ae7061fa5c61c495cc47c30a48784267bb9362
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 01:39:33 +0700

    Menambahkan InsertMhsView untuk menangani tampilan form input data mahasiswa, dengan fungsi untuk memperbarui state di ViewModel, menampilkan snackbar, dan menyimpan data mahasiswa saat tombol simpan ditekan.

---
+++ app/src/main/java/com/example/questroomdatabase/ui/view/mahasiswa/insertMhsView.kt
@@ -0,0 +1,66 @@
+package com.example.questroomdatabase.ui.view.mahasiswa
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(factory = penyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackbarMessage) {
+        uiState.snackbarMessage?.let {message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Mahasiswa",
+                modifier = Modifier
+            )
+            //isi body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = {updateEvent ->
+                    viewModel.updateState(updateEvent) //Update state di viewModel
+                },
+                onClick = {
+                    viewModel.saveData() //Simpan dat
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
