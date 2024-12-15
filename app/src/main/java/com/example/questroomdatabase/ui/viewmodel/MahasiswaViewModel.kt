67469f6177e338cc2884838c3df5090dacbd1aa5
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 00:34:24 +0700

    State untuk menyimpan data yang akan digunakan di UI

--- app/src/main/java/com/example/questroomdatabase/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/questroomdatabase/ui/viewmodel/MahasiswaViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.questroomdatabase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 // ViewModel untuk mengelola data Mahasiswa dan logika UI
 class MahasiswaViewModel(private val repositoryMhs: RepositoryMhs) : ViewModel() {
+    // State untuk menyimpan data yang akan digunakan di UI
+    var uiState by mutableStateOf(MhsUiState())
+        private set
+}
